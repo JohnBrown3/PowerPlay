686d015119dd5adf1f04e03bd10af35b5b331594
AnApostrophe <64292111+AnApostrophe@users.noreply.github.com>
2022-10-08 11:44:00 -0700

    Added Basic Driving
    Done in GitHub, probably has typos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainDriveMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainDriveMode.java
@@ -1,60 +1,86 @@
 
+// Adds FTC libraries
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+// Creates OpMode for Driver Hub
 @TeleOp(name = "MainDriveMode", group = "Linear Opmode")
 public class MainDriveMode extends LinearOpMode 
 {
-  private DcMotor DriveLeft, DriveRight, Carousel, ArmMotor;
-  private Servo LeftClaw, RightClaw, ArmServo;
+  // Declares hardware variables
+  private DcMotor RearLeftWheel, RearRightWheel, FrontLeftWheel, FrontRightWheel;
+  //private DcMotor RearLeftWheel, RearRightWheel, FrontLeftWheel, FrontRightWheel, SpoolMotor;
+  //private Servo ClawServo;
 
-  private double speedDivider = 1.5;
-  private double rightFactor;
-  private double leftFactor;
-  private boolean isLocked = true;
-
+  // When the Start Buttons is pressed
   @Override
   public void runOpMode() 
   {
-    //Maps hardware on robot to previously declared variables
-    DriveLeft = hardwareMap.dcMotor.get("DriveLeft");
-    DriveRight = hardwareMap.dcMotor.get("DriveRight");
-    Carousel = hardwareMap.dcMotor.get("Carousel");
-    ArmMotor = hardwareMap.dcMotor.get("ArmMotor");
-    LeftClaw = hardwareMap.servo.get("LeftClaw");
-    RightClaw = hardwareMap.servo.get("RightClaw");
-    ArmServo = hardwareMap.servo.get("ArmServo");
+    // Maps hardware to hardware variables
+    RearLeftWheel = hardwareMap.dcMotor.get("RearLeftWheel");
+    RearRightWheel = hardwareMap.dcMotor.get("RearRightWheel");
+    FrontLeftWheel = hardwareMap.dcMotor.get("FrontLeftWheel");
+    FrontRightWheel = hardwareMap.dcMotor.get("FrontRightWheel");
+    //SpoolMotor = hardwareMap.dcMotor.get("ArmServo");
+    //ClawServo = hardwareMap.servo.get("ClawServo");
 
     waitForStart();
     
+    // When the Play Button is pressed
     if (opModeIsActive()) 
     {
-      //Initializes modes and encoders for arm servo
-      ArmMotor.setTargetPosition(0);
-      ArmMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-      ArmMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-      ArmMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-      ArmMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+      // Initializes modes and encoders for spool motor
+      SpoolMotor.setTargetPosition(0);
+      SpoolMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+      SpoolMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+      SpoolMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+      SpoolMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
       
-      //Reverses directions for backwards motors/servos
-      DriveRight.setDirection(DcMotorSimple.Direction.REVERSE);
-      RightClaw.setDirection(Servo.Direction.REVERSE);
+      // Reverses directions for backwards motors
+      RearRightWheel.setDirection(DcMotorSimple.Direction.REVERSE);
+      FrontRightWheel.setDirection(DcMotorSimple.Direction.REVERSE);
       
-      DriveRight.setPower(0);
-      DriveLeft.setPower(0);
-      ArmMotor.setTargetPosition(100);
-
-      ArmServo.setPosition(0.25);
+      // Set powers and positions to motors & servos
+      RearLeftWheel.setPower(0);
+      RearRightWheel.setPower(0);
+      FrontLeftWheel.setPower(0);
+      FrontRightWheel.setPower(0);
+      SpoolMotor.setTargetPosition(0);
+      ClawServo.setPosition(0);
       
+      // Runloop
       while (opModeIsActive()) 
       {
-        //Pressing 'mode' on gamepad switches the left joystick and the d-pad controls
+        // Notes:
+        // To set Gamepad as 1 or 2, hold 'Start' and press 'A' or 'B'
+        // Pressing 'mode' on gamepad switches the left joystick and the d-pad controls
 
+        // Movement //
+        // Driver Input
+        float leftStickY = -gamepad1.left_stick_y;
+        float leftStickX = gamepad1.left_stick_x;
+        float rightStickX = gamepad1.right_stick_x;
+
+        // Direction/Power Calculation
+        float denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        float rearLeftPower = (leftStickY - leftStickX + rightStickX) / denominator;
+        float rearRightPower = (leftStickY + leftStickX - rightStickX) / denominator;
+        float frontLeftPower = (leftStickY + leftStickX + rightStickX) / denominator;
+        float frontRightPower = (leftStickY - leftStickX - rightStickX) / denominator;
+        
+        // Direction/Power Assignment
+        RearLeftWheel.setPower(rearLeftPower);
+        RearRightWheel.setPower(rearRightPower);
+        FrontLeftWheel.setPower(frontLeftPower);
+        FrontRightWheel.setPower(frontRightPower);
+
+        // OLD CODE BELOW //
+        /*
+        
         //Holding left trigger speeds up robot
         if(gamepad1.left_trigger == 1)
         {
@@ -160,16 +186,16 @@ public class MainDriveMode extends LinearOpMode
         
         //Manually adjusts arm servo
         ArmServo.setPosition(Math.max(0.25, Math.min(ArmServo.getPosition() + gamepad2.left_stick_y / 150, 0.80)));
-
         
-        /* Telemetry */
-        //Useful data displayed on driver hub for testing
-        telemetry.addData("Left Power", DriveLeft.getPower());
-        telemetry.addData("Right Power", DriveRight.getPower());
-        telemetry.addData("Carousel Power", Carousel.getPower());
-        telemetry.addData("Arm Position", ArmMotor.getCurrentPosition());
-        telemetry.addData("Target Position", ArmMotor.getTargetPosition());
-        telemetry.addData("Arm Servo", ArmServo.getPosition());
+        */
+        
+        // Telemetry displayed on Driver Station
+        telemetry.addData("RearLeftWheel", RearLeftWheel.getPower());
+        telemetry.addData("RearRightWheel", RearRightWheel.getPower());
+        telemetry.addData("FrontLeftWheel", FrontLeftWheel.getPower());
+        telemetry.addData("FrontRightWheel", FrontRightWheel.getPower());
+        //telemetry.addData("SpoolMotorPosition", SpoolMotor.getCurrentPosition());
+        //telemetry.addData("ClawServo", ClawServo.getPosition());
         telemetry.update();
       }
     }
