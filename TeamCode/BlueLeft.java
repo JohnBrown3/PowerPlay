d2e63fd4698c7c4d177568503b2a2bfd64646331
AnApostrophe <64292111+AnApostrophe@users.noreply.github.com>
2022-10-15 12:00:46 -0700

    Auto testing

--- TeamCode/BlueLeft.java
+++ TeamCode/BlueLeft.java
@@ -1,34 +1,80 @@
 
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.common.powerplay.SleeveDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
 @Autonomous(name = "BlueLeft", group = "Linear Opmode")
 public class BlueLeft extends LinearOpMode
 {
+    SleeveDetection sleeveDetection = new SleeveDetection();
+    OpenCvCamera camera;
+    String webcamName = "Webcam 1"
+
     //Hardware
     private DcMotor RearLeftWheel, RearRightWheel, FrontLeftWheel, FrontRightWheel, SpoolMotor;
     //private Servo ClawServo;
+    private BNO055IMU imu;
 
     //Numbers
+    private Orientation lastAngles = new Orientation();
+
     private int rearLeftPosition, rearRightPosition, frontLeftPosition, frontRightPosition;
     private double targetTime;
 
     static final double conversionFactor = 3000/44;
+    static final double turnSpeed = 1;
 
     @Override
     public void runOpMode()
     {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, webcamName), cameraMonitorViewId);
+        sleeveDetection = new SleeveDetection();
+        camera.setPipeline(sleeveDetection);
+
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(300,400, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {}
+        });
+
         //Assignment
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
         RearLeftWheel = hardwareMap.dcMotor.get("RearLeftWheel");
         RearRightWheel = hardwareMap.dcMotor.get("RearRightWheel");
         FrontLeftWheel = hardwareMap.dcMotor.get("FrontLeftWheel");
         FrontRightWheel = hardwareMap.dcMotor.get("FrontRightWheel");
         SpoolMotor = hardwareMap.dcMotor.get("ArmServo");
         //ClawServo = hardwareMap.servo.get("ClawServo");
+
+        BNO055IMU.Parameters imuParameters = new BNO055IMU.Parameters();
+        imuParameters.mode = BNO055IMU.SensorMode.IMU;
+        imuParameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        imuParameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        imuParameters.loggingEnabled = false;
+
+        imu.initialize(imuParameters);
+
+        while (!isStarted()) {
+            telemetry.addData("ROTATION: ", sleeveDetection.getPosition());
+            telemetry.update();
+        }
 
         waitForStart();
 
@@ -37,7 +83,9 @@ public class BlueLeft extends LinearOpMode
             //Initialization
             InitializeHardware();
 
-
+            VerticalDrive(6);
+            Turn(90);
+            HorizontalDrive(6);
         }
     }
 
@@ -51,42 +99,124 @@ public class BlueLeft extends LinearOpMode
         }
     }
 
-    private void VerticalMovement(double target) {
-        rearLeftPosition += (int) (target);
-        rearRightPosition += (int) (target);
-        frontLeftPosition += (int) (target);
-        frontRightPosition += (int) (target);
+    private void VerticalDrive (double target)
+    {
+        rearLeftPosition += (int) (target * conversionFactor);
+        rearRightPosition += (int) (target * conversionFactor);
+        frontLeftPosition += (int) (target * conversionFactor);
+        frontRightPosition += (int) (target * conversionFactor);
 
-
-        RearLeftWheel.setTargetPosition();
-        RearRightWheel.setTargetPosition();
-        FrontLeftWheel.setTargetPosition();
-        FrontRightWheel.setTargetPosition();
-    }
-
-    private void Drive(double leftTarget, double rightTarget) {
-
-        leftPosition += (int) (leftTarget * conversionFactor * leftFactor);
-        rightPosition += (int) (rightTarget * conversionFactor * rightFactor);
-
-        DriveLeft.setTargetPosition(leftPosition);
-        DriveRight.setTargetPosition(rightPosition);
-
-        DriveLeft.setPower(0.5 * leftFactor);
-        DriveRight.setPower(0.5 * rightFactor);
+        RearLeftWheel.setTargetPosition(rearLeftPosition);
+        RearRightWheel.setTargetPosition(rearRightPosition);
+        FrontLeftWheel.setTargetPosition(frontLeftPosition);
+        FrontRightWheel.setTargetPosition(frontRightPosition);
 
         while (DriveLeft.isBusy() && DriveRight.isBusy()) {
             idle();
         }
     }
 
-    private void InitializeHardware() {
+    private void HorizontalDrive (double target)
+    {
+        rearLeftPosition -= (int) (target * conversionFactor);
+        rearRightPosition += (int) (target * conversionFactor);
+        frontLeftPosition += (int) (target * conversionFactor);
+        frontRightPosition -= (int) (target * conversionFactor);
+
+        RearLeftWheel.setTargetPosition(rearLeftPosition);
+        RearRightWheel.setTargetPosition(rearRightPosition);
+        FrontLeftWheel.setTargetPosition(frontLeftPosition);
+        FrontRightWheel.setTargetPosition(frontRightPosition);
+
+        while (DriveLeft.isBusy() && DriveRight.isBusy()) {
+            idle();
+        }
+    }
+
+    private void Turn (int targetDegree)
+    {
+        ResetPosition();
+
+        RearLeftWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        RearRightWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FrontLeftWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FrontRightWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        double leftPower, rightPower;
+
+        Orientation currentAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        double deltaAngle = targetDegree - currentAngles.firstAngle;
+
+        if(deltaAngle < -180)
+        {
+            deltaAngle += 360;
+        }
+        else if(deltaAngle > 180)
+        {
+            deltaAngle -= 360;
+        }
+
+        if(deltaAngle > 0)
+        {
+            leftPower = -turnSpeed;
+            rightPower = turnSpeed;
+        }
+        else if(deltaAngle < 0)
+        {
+            leftPower = turnSpeed;
+            rightPower = -turnSpeed;
+        }
+        else
+        {
+            return;
+        }
+
+        RearLeftWheel.setPower(leftPower);
+        FrontLeftWheel.setPower(leftPower);
+        RearRightWheel.setPower(rightPower);
+        FrontRightWheel.setPower(rightPower);
+
+        while(opModeIsActive() && Math.abs(deltaAngle) > 1)
+        {
+            currentAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            deltaAngle = targetDegree - currentAngles.firstAngle;
+
+            if(deltaAngle < -180)
+            {
+                deltaAngle += 360;
+            }
+            else if(deltaAngle > 180)
+            {
+                deltaAngle -= 360;
+            }
+
+            idle();
+        }
+
+        RearLeftWheel.setPower(0);
+        FrontLeftWheel.setPower(0);
+        RearRightWheel.setPower(0);
+        FrontRightWheel.setPower(0);
+
+        ResetPosition();
+
+        InitializeWheels();
+    }
+
+    private void InitializeHardware ()
+    {
         SpoolMotor.setTargetPosition(0);
         SpoolMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         SpoolMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         SpoolMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         SpoolMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        InitializeWheels();
+    }
+
+    private void InitializeWheels ()
+    {
         RearLeftWheel.setTargetPosition(0);
         RearLeftWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         RearLeftWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -112,4 +242,3 @@ public class BlueLeft extends LinearOpMode
         FrontRightWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 }
-
--- TeamCode/MainDriveMode.java
+++ TeamCode/MainDriveMode.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 // Creates OpMode for Driver Hub
 @TeleOp(name = "MainDriveMode", group = "Linear Opmode")
-public class MainDriveMode extends LinearOpMode 
+public class MainDriveMode extends LinearOpMode
 {
   // Declares hardware variables
   private DcMotor RearLeftWheel, RearRightWheel, FrontLeftWheel, FrontRightWheel, SpoolMotor;
@@ -17,24 +17,26 @@ public class MainDriveMode extends LinearOpMode
 
   // When the Start Buttons is pressed
   @Override
-  public void runOpMode() 
+  public void runOpMode()
   {
     // Maps hardware to hardware variables
     RearLeftWheel = hardwareMap.dcMotor.get("RearLeftWheel");
     RearRightWheel = hardwareMap.dcMotor.get("RearRightWheel");
     FrontLeftWheel = hardwareMap.dcMotor.get("FrontLeftWheel");
     FrontRightWheel = hardwareMap.dcMotor.get("FrontRightWheel");
-    SpoolMotor = hardwareMap.dcMotor.get("ArmServo");
+    SpoolMotor = hardwareMap.dcMotor.get("SpoolMotor");
     ClawServo = hardwareMap.servo.get("ClawServo");
 
     // Reverse right motors
     RearRightWheel.setDirection(DcMotorSimple.Direction.REVERSE);
     FrontRightWheel.setDirection(DcMotorSimple.Direction.REVERSE);
+    SpoolMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    ClawServo.setDirection(Servo.Direction.REVERSE);
 
     waitForStart();
-    
+
     // When the Play Button is pressed
-    if (opModeIsActive()) 
+    if (opModeIsActive())
     {
       // Initializes modes and encoders for spool motor
       SpoolMotor.setTargetPosition(0);
@@ -42,11 +44,11 @@ public class MainDriveMode extends LinearOpMode
       SpoolMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
       SpoolMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
       SpoolMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-      
+
       // Reverses directions for backwards motors
       RearRightWheel.setDirection(DcMotorSimple.Direction.REVERSE);
       FrontRightWheel.setDirection(DcMotorSimple.Direction.REVERSE);
-      
+
       // Set powers and positions to motors & servos
       RearLeftWheel.setPower(0);
       RearRightWheel.setPower(0);
@@ -55,9 +57,9 @@ public class MainDriveMode extends LinearOpMode
       SpoolMotor.setPower(0);
       SpoolMotor.setTargetPosition(0);
       //ClawServo.setPosition(0);
-      
+
       // Runloop
-      while (opModeIsActive()) 
+      while (opModeIsActive())
       {
         /* Notes */
         // To set Gamepad as 1 or 2, hold 'Start' and press 'A' or 'B'
@@ -75,7 +77,7 @@ public class MainDriveMode extends LinearOpMode
         float rearRightPower = (leftStickY + leftStickX - rightStickX) / denominator;
         float frontLeftPower = (leftStickY + leftStickX + rightStickX) / denominator;
         float frontRightPower = (leftStickY - leftStickX - rightStickX) / denominator;
-        
+
         // Direction/Power Assignment
         RearLeftWheel.setPower(rearLeftPower);
         RearRightWheel.setPower(rearRightPower);
@@ -85,36 +87,36 @@ public class MainDriveMode extends LinearOpMode
         /* Slide Control */
         // Manual slide control
         SpoolMotor.setPower(1);
-        SpoolMotor.setTargetPosition(ArmMotor.getTargetPosition() - (int)(gamepad2.right_stick_y * 6));
+        SpoolMotor.setTargetPosition(SpoolMotor.getTargetPosition() - (int)(gamepad2.right_stick_y * 10));
 
         // Preset Positions
         if (gamepad2.x)
         {
-            SpoolMotor.setTargetPosition(0);
+          SpoolMotor.setTargetPosition(0);
         }
         else if (gamepad2.a)
         {
-            SpoolMotor.setTargetPosition(550);
+          SpoolMotor.setTargetPosition(2000);
         }
         else if (gamepad2.b)
         {
-            SpoolMotor.setTargetPosition(1100);
+          SpoolMotor.setTargetPosition(4000);
         }
         else if (gamepad2.y)
         {
-            SpoolMotor.setTargetPosition(1600);
+          SpoolMotor.setTargetPosition(8000);
         }
 
         // Servo Control //
         if (gamepad2.right_trigger != 0)
         {
-            ClawServo.setPosition(0.50);
+          ClawServo.setPosition(0);
         }
         else
         {
-            ClawServo.setPosition(0);
+          ClawServo.setPosition(0.25);
         }
-        
+
         /* Telemetry */
         // Displayed on the Driver Station's right panel
         telemetry.addData("RearLeftWheel", RearLeftWheel.getPower());
